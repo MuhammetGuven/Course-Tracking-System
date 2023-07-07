b212acc1b50eb0851085b1e8873d4e29c1e307cf
muhammetguven <96919887+MuhammetGuven@users.noreply.github.com>
2023-07-07 20:50:52 +0300

    Add files via upload

---
+++ Course_Tracking_System-master/AnaSayfa.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VeriYapilari;
+
+namespace ders_takip_sistemi
+{
+    public partial class AnaSayfa : Form
+    {
+        public HashTable dersler;
+        public OgrenciAVL ogrenciListe;
+        public OgretimGorevlisiAVL ogretmenListe;
+
+        public AnaSayfa(HashTable dersler, OgrenciAVL ogrenciListe, OgretimGorevlisiAVL ogretmenListe)
+        {
+            this.dersler = dersler;
+            this.ogrenciListe = ogrenciListe;
+            this.ogretmenListe = ogretmenListe;
+            InitializeComponent();
+        }
+
+        
+        private void label6_Click(object sender, EventArgs e)
+        {
+            duyurular x = new duyurular(dersler, ogrenciListe, ogretmenListe);
+            x.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Giris s = new Giris(dersler, ogrenciListe, ogretmenListe);
+            s.Show();
+            this.Hide();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\2.jpg");
+
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\3.jpg");
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\1.jpg");
+
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox2.Checked = true;
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\2.jpg");
+            }
+            else if (checkBox2.Checked)
+            {
+                checkBox2.Checked = false;
+                checkBox1.Checked = false;
+                checkBox3.Checked = true;
+
+                pictureBox2.ImageLocation = string.Format(@"images\3.jpg");
+
+            }
+            else {
+
+
+                checkBox2.Checked = false;
+                checkBox1.Checked = true;
+                checkBox3.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\1.jpg");
+
+
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox3.Checked = true;
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\3.jpg");
+            }
+            else if (checkBox2.Checked)
+            {
+                checkBox2.Checked = false;
+                checkBox1.Checked = true;
+                checkBox3.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\1.jpg");
+
+            }
+            else
+            {
+
+
+                checkBox2.Checked = true;
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+
+                pictureBox2.ImageLocation = string.Format(@"images\2.jpg");
+
+
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            etkinlikler c = new etkinlikler(dersler, ogrenciListe, ogretmenListe);
+            c.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label6_MouseEnter(object sender, EventArgs e)
+        {
+            
+            label6.ForeColor = Color.Red;
+
+        }
+
+        private void label6_MouseLeave(object sender, EventArgs e)
+        {
+           
+            label6.ForeColor = Color.Black;
+        }
+
+        private void label7_MouseEnter(object sender, EventArgs e)
+        {
+            label7.ForeColor = Color.Red;
+        }
+
+        private void label7_MouseLeave(object sender, EventArgs e)
+        {
+            label7.ForeColor = Color.Black;
+        }
+
+        private void label8_MouseEnter(object sender, EventArgs e)
+        {
+            label8.ForeColor = Color.Red;
+        }
+
+        private void label8_MouseLeave(object sender, EventArgs e)
+        {
+            label8.ForeColor = Color.Black;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            hakkinda x = new hakkinda(dersler, ogrenciListe, ogretmenListe);
+            x.Show();
+            this.Hide();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Course_Tracking_System-master/Program.cs
@@ -0,0 +1,148 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VeriYapilari;
+
+
+// Muhammet Güven 032190025
+// Umut Can Oral 032190063
+// Münir Aydemir 032190045
+// Mehmet Kaan Genç 032190023
+// Lokmane Boulbadaoui 032090090
+// Chaimaa Nairi 031890119
+// Şeyma İlgün 032190068
+// Cansu Kocaoğlu 032090049
+// Ayçanur Güç 032190055 
+// Beyza Tuncel 032190015
+// Sevim Ulusoy 032190013
+// Nisa Büyüktaş 032090058
+
+
+namespace ders_takip_sistemi
+{
+
+    internal static class Program
+    {
+        public static HashTable dersler;
+        public static OgrenciAVL ogrenciListe;
+        public static OgretimGorevlisiAVL ogretmenListe;
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("deneme");
+            dersler = new HashTable(12,20);
+            ogrenciListe = new OgrenciAVL();
+            ogretmenListe = new OgretimGorevlisiAVL();
+
+            string dersAdi, gunId, dersTuru;
+            int ortmenId, dersId, saatId, donemId, dersKredi;
+            string connString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=12345;";
+            NpgsqlConnection conn = new NpgsqlConnection(connString);
+            conn.Open();
+            string sql = "SELECT * FROM ders";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                dersAdi = string.Join("", (string[])reader.GetValue(1));
+                gunId = string.Join("", (string[])reader.GetValue(6));
+                dersTuru = string.Join("", (string[])reader.GetValue(7));
+                ortmenId = reader.GetInt32(0);
+                dersId = reader.GetInt32(3);
+                saatId = reader.GetInt32(4);
+                donemId = reader.GetInt32(5);
+                dersKredi = reader.GetInt32(2);
+                dersler.Ekle(new Ders(dersAdi, gunId, dersTuru, ortmenId, dersId, saatId, donemId, dersKredi));
+            }
+
+            reader.Close();
+
+            sql = "SELECT * FROM OgrenciBilgi";
+            cmd = new NpgsqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+
+            string fullAdi, bolumu, cinsiyet;
+            int ogrenciId;
+            while (reader.Read())
+            {
+                fullAdi = string.Join("", (string[])reader.GetValue(1));
+                bolumu = string.Join("", (string[])reader.GetValue(2));
+                cinsiyet = string.Join("", (string[])reader.GetValue(3));
+                ogrenciId = reader.GetInt32(0);
+                donemId = reader.GetInt32(4);
+                ogrenciListe.InsertOgr(new Ogrenci(fullAdi, cinsiyet, ogrenciId, donemId, "", "", bolumu));
+            }
+
+            reader.Close();
+
+            sql = "SELECT * FROM ogrencigiris";
+            cmd = new NpgsqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+
+            string kullaniciAdi, sifre;
+            Ogrenci ogrenciTemp;
+            while (reader.Read())
+            {
+                ogrenciId = reader.GetInt32(0);
+                kullaniciAdi = reader.GetValue(1).ToString();
+                sifre = reader.GetValue(2).ToString();
+                ogrenciTemp = ogrenciListe.FindOgrenciID(ogrenciId).Ogrenci;
+                ogrenciTemp.kullanici_adi = kullaniciAdi;
+                ogrenciTemp.sifre=sifre;
+            }
+
+            reader.Close();
+
+            sql = "SELECT * FROM ogretimgorevlisibilgi";
+            cmd = new NpgsqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                fullAdi = string.Join("", (string[])reader.GetValue(1));
+                cinsiyet = string.Join("", (string[])reader.GetValue(2));
+                ortmenId = reader.GetInt32(0);
+
+                ogretmenListe.InsertGorevli(new OgretimGorevlisi(fullAdi,cinsiyet,"","",ortmenId));
+            }
+
+            reader.Close();
+
+            sql = "SELECT * FROM ogretmengiris";
+            cmd = new NpgsqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+
+            OgretimGorevlisi ogretimTemp;
+            while (reader.Read())
+            {
+                ortmenId = reader.GetInt32(0);
+                kullaniciAdi = reader.GetValue(1).ToString();
+                sifre = reader.GetValue(2).ToString();
+                ogretimTemp = ogretmenListe.FindGorevliID(ortmenId).OgretimGorevlisi;
+                ogretimTemp.kullanici_adi = kullaniciAdi;
+                ogretimTemp.sifre = sifre;
+            }
+            reader.Close();
+            conn.Close();
+
+            Liste x;
+
+            for (int i = 0; i < dersler.hkapasite; i++)
+            {
+                x = dersler.ListBul(i+10);
+                x.ElamanYaz();
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new loadingPage(dersler,ogrenciListe,ogretmenListe));
+
+            
+        }
+    }
+}
+ 
